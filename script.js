d071b4ea6cb82b32e85a3e29b9eddc748e13e83b
Vinh <tranthanhvinhlxag1995@gmail.com>
2023-11-22 21:02:26 +0700

    update function to open modal

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ const modal = document.querySelector(`.modal`);
 const showModal = document.querySelectorAll(`.show-modal`);
 const overLay = document.querySelector(`.overlay`);
 const closeModal = document.querySelector(`.close-modal`);
-for(let i = 0; i < showModal.length; i++){
-    showModal[i].addEventListener(`click`, function(){
-        modal.classList.remove(`hidden`);
+const open = function (){
+    modal.classList.remove(`hidden`);
         overLay.classList.remove(`hidden`);
-    })
+}
+for(let i = 0; i < showModal.length; i++){
+    showModal[i].addEventListener(`click`, open);
 }
 /*
  closeModal.addEventListener(`click`,() => {modal.classList.add(`hidden`);
